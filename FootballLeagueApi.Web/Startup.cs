7ed1c3c0e372ea1db26ab4a84cbd9f9d7b7dbffa
MilenDinev <dinev.milen@gmail.com>
2023-12-02 20:12:01 +0200

    JWT Token implementation

--- FootballLeagueApi.Web/Startup.cs
+++ FootballLeagueApi.Web/Startup.cs
@@ -1,22 +1,15 @@
-using FootballLeagueApi.Data.Seeders;
-using FootballLeagueApi.Web.ConfigExtensions;
-using FootballLeagueApi.Web.Middlewares;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace FootballLeagueApi.Web
 {
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Utils;
+    using Middlewares;
+    using ConfigExtensions;
+    using Data.Seeders;
+
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -29,13 +22,11 @@ namespace FootballLeagueApi.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddJwtAuthentication(Configuration);
+            services.AddControllers();
             services.AddDatabase(Configuration);
             services.RegisterServices();
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "FootballLeagueApi.Web", Version = "v1" });
-            });
+            services.AddSwagger();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,6 +47,7 @@ namespace FootballLeagueApi.Web
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
---
+++ FootballLeagueApi.Web/Utils/ApplicationService.cs
@@ -0,0 +1,66 @@
+﻿namespace FootballLeagueApi.Web.Utils
+{
+    using Microsoft.AspNetCore.Authentication.JwtBearer;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.IdentityModel.Tokens;
+    using Microsoft.OpenApi.Models;
+    using System.Text;
+
+    public static class ApplicationService
+    {
+        public static void AddJwtAuthentication(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication(k =>
+            {
+                k.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                k.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(p =>
+            {
+                var key = Encoding.UTF8.GetBytes(configuration["JWTToken:key"]);
+                p.SaveToken = true;
+                p.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateLifetime = false,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = configuration["JWKToken:key"],
+                    ValidAudience = configuration["JWKToken:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(key)
+                };
+            });
+        }
+
+        public static void AddSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(option =>
+            {
+                option.SwaggerDoc("v1", new OpenApiInfo { Title = "FootballLeagueApi.Web", Version = "v1" });
+                option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter a valid token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "Bearer"
+                });
+                option.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[]{}
+                    }
+                });
+            });
+        }
+    }
+}
