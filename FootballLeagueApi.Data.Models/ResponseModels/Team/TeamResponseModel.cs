97344a3a78ac74a67163e3bd94af2dd0fce702a2
MilenDinev <dinev.milen@gmail.com>
2023-12-02 12:09:29 +0200

    Initial properties creation

--- FootballLeagueApi.Data.Models/ResponseModels/Team/TeamResponseModel.cs
+++ FootballLeagueApi.Data.Models/ResponseModels/Team/TeamResponseModel.cs
@@ -2,5 +2,15 @@
 {
     public class TeamResponseModel
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Points { get; set; }
+        public int TotalGamesPlayed { get; set; }
+        public int HomeGamesPlayed { get; set; }
+        public int AwayGamesPlayed { get; set; }
+        public int GamesWon { get; set; }
+        public int GamesLost { get; set; }
+        public int GamesDraw { get; set; }
+        public int TotalGoalScored { get; set; }
     }
 }
