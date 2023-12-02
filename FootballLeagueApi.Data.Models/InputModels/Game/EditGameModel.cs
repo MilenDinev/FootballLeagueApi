9ecbb02de38df9d914682d6e2a3355c85e166700
MilenDinev <dinev.milen@gmail.com>
2023-12-02 12:08:42 +0200

    Making int properties nullable

--- FootballLeagueApi.Data.Models/InputModels/Game/EditGameModel.cs
+++ FootballLeagueApi.Data.Models/InputModels/Game/EditGameModel.cs
@@ -1,15 +1,23 @@
 ﻿namespace FootballLeagueApi.Data.Models.InputModels.Game
 {
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using Constants;
+
     public class EditGameModel
     {
-        public int HomeTeamId { get; set; }
-        public int AwayTeamId { get; set; }
-        public int HomeTeamGoals { get; set; }
+        public int? HomeTeamId { get; set; }
+        public int? AwayTeamId { get; set; }
+
         [Range(AttributeParams.MinGoals, 
             AttributeParams.MaxGoals, 
             ErrorMessage = ValidationMessages.MinMaxGoals)]
+        public int? HomeTeamGoals { get; set; }
+
         [Range(AttributeParams.MinGoals, 
             AttributeParams.MaxGoals, 
             ErrorMessage = ValidationMessages.MinMaxGoals)]
+        public int? AwayTeamGoals { get; set; }
+        public string PlayedOn { get; set; }
     }
 }
