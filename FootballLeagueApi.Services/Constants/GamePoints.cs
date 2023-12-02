07c145fdc0666eb5d04cf027c6c74fe25c4a1945
MilenDinev <dinev.milen@gmail.com>
2023-12-02 15:57:38 +0200

    Initial Creation

---
+++ FootballLeagueApi.Services/Constants/GamePoints.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballLeagueApi.Services.Constants
+{
+    public static class GamePoints
+    {
+        public const int Win = 3;
+        public const int Draw = 1;
+    }
+}
