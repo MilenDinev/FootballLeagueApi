5767335df4d493cb259b8ef5d8c26dd6e5ce6861
MilenDinev <dinev.milen@gmail.com>
2023-12-02 15:57:30 +0200

    Updating EntityDoesNotExist message

--- FootballLeagueApi.Services/Constants/ErrorMessages.cs
+++ FootballLeagueApi.Services/Constants/ErrorMessages.cs
@@ -2,7 +2,7 @@
 {
     public static class ErrorMessages
     {
-        public const string EntityDoesNotExist = @"{0} with {1} '{2}' does not exist!";
+        public const string EntityDoesNotExist = @"{0} with {1} '{2}' does not exists!";
         public const string EntityAlreadyExists = @"{0} - '{1}' already exists!";
         public const string SameTeam = @"Home Team id and Away Team id should be different!";
     }
