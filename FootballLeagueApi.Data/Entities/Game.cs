bd9a1bde453db82fffa0902f43ce060e54907504
MilenDinev <dinev.milen@gmail.com>
2023-12-02 15:55:38 +0200

    Removing PlayedOn property

--- FootballLeagueApi.Data/Entities/Game.cs
+++ FootballLeagueApi.Data/Entities/Game.cs
@@ -11,6 +11,5 @@
         public virtual Team AwayTeam { get; set; }
         public int HomeTeamGoals { get; set; }
         public int AwayTeamGoals { get; set; }
-        public DateTime PlayedOn { get; set; }
     }
 }
