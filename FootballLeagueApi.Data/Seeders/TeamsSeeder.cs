85957fef2e9b0adab096041016ac0453c9c53726
MilenDinev <dinev.milen@gmail.com>
2023-12-02 16:06:21 +0200

    Initializing NormalizedTag

--- FootballLeagueApi.Data/Seeders/TeamsSeeder.cs
+++ FootballLeagueApi.Data/Seeders/TeamsSeeder.cs
@@ -13,14 +13,16 @@
             {
                 new Team()
                 {
-                    Name = "Levski Sofia",
+                    Name = "Levski",
+                    NormalizedTag = "Levski".ToUpper(),
                     CreationDate = DateTime.UtcNow,
                     LastModifiedOn = DateTime.UtcNow
                 },
 
                 new Team()
                 {
-                    Name = "CSKA Sofia",
+                    Name = "CSKA",
+                    NormalizedTag = "CSKA".ToUpper(),
                     CreationDate = DateTime.UtcNow,
                     LastModifiedOn = DateTime.UtcNow
                 },
@@ -28,6 +30,7 @@
                 new Team()
                 {
                     Name = "Slavia",
+                    NormalizedTag = "Slavia".ToUpper(),
                     CreationDate = DateTime.UtcNow,
                     LastModifiedOn = DateTime.UtcNow
                 },
@@ -35,6 +38,7 @@
                 new Team()
                 {
                     Name = "Beroe",
+                    NormalizedTag = "Beroe".ToUpper(),
                     CreationDate = DateTime.UtcNow,
                     LastModifiedOn = DateTime.UtcNow
                 },
@@ -42,20 +46,7 @@
                 new Team()
                 {
                     Name = "Ludogorets",
-                    CreationDate = DateTime.UtcNow,
-                    LastModifiedOn = DateTime.UtcNow
-                },
-
-                new Team()
-                {
-                    Name = "Lokomotiv Plovdiv",
-                    CreationDate = DateTime.UtcNow,
-                    LastModifiedOn = DateTime.UtcNow
-                },
-
-                new Team()
-                {
-                    Name = "Lokomotiv Sofia",
+                    NormalizedTag = "Ludogorets".ToUpper(),
                     CreationDate = DateTime.UtcNow,
                     LastModifiedOn = DateTime.UtcNow
                 },
@@ -63,20 +54,15 @@
                 new Team()
                 {
                     Name = "Marek",
+                    NormalizedTag = "Marek".ToUpper(),
                     CreationDate = DateTime.UtcNow,
                     LastModifiedOn = DateTime.UtcNow
                 },
 
                 new Team()
                 {
-                    Name = "Botev Plovdiv",
-                    CreationDate = DateTime.UtcNow,
-                    LastModifiedOn = DateTime.UtcNow
-                },
-
-                new Team()
-                {
-                    Name = "Cherno More",
+                    Name = "Chernomorets",
+                    NormalizedTag = "Chernomorets".ToUpper(),
                     CreationDate = DateTime.UtcNow,
                     LastModifiedOn = DateTime.UtcNow
                 },
