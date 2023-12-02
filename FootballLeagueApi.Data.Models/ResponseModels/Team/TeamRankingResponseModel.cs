8459a49179f3d87a148b9af19e557a18aeb988d7
MilenDinev <dinev.milen@gmail.com>
2023-12-02 20:01:33 +0200

    initial creation

---
+++ FootballLeagueApi.Data.Models/ResponseModels/Team/TeamRankingResponseModel.cs
@@ -0,0 +1,17 @@
+﻿namespace FootballLeagueApi.Data.Models.ResponseModels.Team
+{
+    public class TeamRankingResponseModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Rank { get; set; }
+        public int Points { get; set; }
+        public int TotalGamesPlayed { get; set; }
+        public int HomeGamesPlayed { get; set; }
+        public int AwayGamesPlayed { get; set; }
+        public int GamesWon { get; set; }
+        public int GamesLost { get; set; }
+        public int GamesDraw { get; set; }
+        public int TotalGoalScored { get; set; }
+    }
+}
